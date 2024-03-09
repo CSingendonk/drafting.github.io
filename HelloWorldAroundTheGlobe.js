9b3f8d7f99d059014dbe0dc66dcc970119af8ae9
CSingendonk <149417592+CSingendonk@users.noreply.github.com>
2024-03-08 23:33:37 -0700

    Update HelloWorldAroundTheGlobe.js

--- HelloWorldAroundTheGlobe.js
+++ HelloWorldAroundTheGlobe.js
@@ -1,5 +1,10 @@
 let isTalkingShit = false;
-
+  if (!document.getElementById("hellofrom")){
+    let hey = document.createElement('h1');
+    hey.id = "hellofrom";
+    hey.textContent = "Hey There!";
+    document.getElementsByTagName("body")[0].appendChild(hey);
+  }
 function loadedthepage(){
   if (!document.getElementById("hellofrom")){
     let hey = document.createElement('h1');
